fb8ff6f7f89d8a57c7504bd8c8b7b3fd0f63189f
MrMelon <github@onpointcoding.net>
2021-06-09 17:06:40 +0100

    Only plantable on podzol

--- src/main/java/net/onpointcoding/redstone_sapling/blocks/RedstoneSaplingBlock.java
+++ src/main/java/net/onpointcoding/redstone_sapling/blocks/RedstoneSaplingBlock.java
@@ -1,6 +1,7 @@
 package net.onpointcoding.redstone_sapling.blocks;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SaplingBlock;
 import net.minecraft.block.sapling.SaplingGenerator;
 import net.minecraft.util.math.BlockPos;
@@ -10,6 +11,11 @@ import net.minecraft.world.BlockView;
 public class RedstoneSaplingBlock extends SaplingBlock {
     public RedstoneSaplingBlock(SaplingGenerator generator, Settings settings) {
         super(generator, settings);
+    }
+
+    @Override
+    protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+        return floor.isOf(Blocks.PODZOL);
     }
 
     @Override
